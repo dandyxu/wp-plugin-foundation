73fa4d1adf3e976483109dbf52e0c7326db44f01
dandyxu <dandyjefferson@gmail.com>
2018-01-29 11:47:00 +0000

    Initial Project

---
+++ wp-plugin-foundation.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: WP Plugin Foundation
+ * Plugin URI: https://github.com/dandyxu/wp-plugin-foundation
+ * Description: My Personal WordPress Plugin Foundation
+ * Version: 1.0
+ * Author: Dandy Xu
+ * Author URI: https://dandyxu.me
+ * License: GPLv2
+ */
